8697683f6d372440f1d84727f3c7ebf60e5c285f
memerelics <takuya21hashimoto@gmail.com>
2014-08-21 16:13:17 +0900

    [RailsCasts] classify / get video link from source

--- railscasts.rb
+++ railscasts.rb
@@ -3,56 +3,88 @@ Bundler.require(:default, :railscasts)
 
 require 'yaml'
 require 'capybara/poltergeist'
+require 'open-uri'
 
-@conf = YAML.load(open('secret.yml').read)
-cookies_file = 'cookies_railscasts.txt'
+class RailsCasts
+  attr_reader :conf, :cookies_file, :page
 
-Capybara.javascript_driver = :webkit
-@page = Capybara::Session.new(:webkit)
+  def initialize
+    @conf = YAML.load(open('secret.yml').read)
+    @cookies_file = 'cookies_railscasts.txt'
+    Capybara.javascript_driver = :webkit
+    @page = Capybara::Session.new(:webkit)
+    load_cookies
+    @page.visit 'http://railscasts.com/'
+  end
 
-open(cookies_file).each do |l|
-  @page.driver.browser.set_cookie(l.chomp)
-end
+  def load_cookies
+    open(@cookies_file).each do |l|
+      @page.driver.browser.set_cookie(l.chomp)
+    end
+  end
 
-@page.visit 'http://railscasts.com/'
+  def login
+    @page.click_link 'Sign in through GitHub'
 
-def login_and_save_cookies
-  @page.click_link 'Sign in through GitHub'
+    @page.fill_in('login', with: @conf['github']['login'])
+    @page.fill_in('password', with: @conf['github']['password'])
+    @page.find(:css, '[name=commit]').click
 
-  @page.fill_in('login', with: @conf['github']['login'])
-  @page.fill_in('password', with: @conf['github']['password'])
-  @page.find(:css, '[name=commit]').click
+    # Two-factor Verification.
+    print "Verification Code: "; input = gets.strip
+    @page.fill_in 'otp', with: input
+    @page.click_button 'Verify'
+    true
+  end
 
-  # Two-factor Verification.
-  print "Verification Code: "; input = gets.strip
-  @page.fill_in 'otp', with: input
-  @page.click_button 'Verify'
+  def save_cookies
+    open(@cookies_file, 'w+') do |f|
+      @page.driver.browser.get_cookies.each do |cookie|
+        f.puts cookie
+      end
+    end
+  end
 
-  open(cookies_file, 'w+') do |f|
-    @page.driver.browser.get_cookies.each do |cookie|
-      f.puts cookie
+  def scrape_names
+    names = []
+    # loop do
+    2.times do
+      names += @page.driver.find_css('.episode .screenshot a')
+                 .map{|a| a['href'].gsub(%r|/episodes/|, '') }
+      begin
+        @page.click_link 'Next Page >'
+        sleep 2
+      rescue => e
+        puts e
+        break
+      end
+    end
+    return names
+  end
+
+  def save_movie_links(file)
+    login && save_cookies if !@page.has_content?('Logged in')
+    open(file, 'w+') do |f|
+      scrape_names.each do |name|
+        @page.visit "http://railscasts.com/episodes/#{name}?autoplay=true"
+        doc = Nokogiri::XML(@page.source)
+        f.puts doc.css('video source')[0]['src']
+      end
+    end
+  end
+
+  def download_movies(links_file, target)
+    system "mkdir -p #{target}"
+    open(links_file).each do |line|
+      # filename = line.chomp.gsub(%r|^.*/(\d+-[\w_-]+.mp4)$|) { $1 }
+      # binding.pry
+      # open("#{target}/#{filename}", 'wb') do |file|
+      #   file << open(line.chomp).read
+      # end
     end
   end
 end
 
-login_and_save_cookies if !@page.has_content?('Logged in')
-
-names = []
-# loop do
-5.times do
-  names += @page.driver.find_css('.episode .screenshot a').map{|a| a['href'].gsub(%r|/episodes/|, '') }
-  begin
-    @page.click_link 'Next Page >'
-  rescue => e
-    puts e
-    break
-  end
-end
-
-open('movie_links.txt', 'w+') do |f|
-  names.each do |name|
-    f.puts "http://media.railscasts.com/assets/episodes/videos/#{name}.mp4"
-  end
-end
-
-# binding.pry
+rc = RailsCasts.new
+rc.save_movie_links('movie_links.txt')
+# rc.download_movies('2_movie_links.txt', 'movies')
