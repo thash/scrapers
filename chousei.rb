b2de526b2697dac0eca69ac74ec6d19e047b39a6
memerelics <takuya21hashimoto@gmail.com>
2014-09-10 18:17:57 +0900

    調整さん for Bio x IT論文輪読会

---
+++ chousei.rb
@@ -0,0 +1,36 @@
+require 'bundler/setup'
+Bundler.require(:default, :railscasts)
+require 'active_support/core_ext/date/calculations'
+
+Capybara.javascript_driver = :webkit
+@page = Capybara::Session.new(:webkit)
+
+def num(date)
+  ((date.year - 2014) * 54) + date.cweek + 35
+end
+
+def kouho(start_date)
+  str = ""
+  (start_date..start_date + 6).each do |date|
+    wd = %w(日 月 火 水 木 金 土)[date.wday]
+    if %w(土 日).include?(wd)
+      str << date.strftime("%m月%d日(#{wd}) 10:00-\n")
+      str << date.strftime("%m月%d日(#{wd}) 22:00-\n")
+    else
+      str << date.strftime("%m月%d日(#{wd}) 22:30-\n")
+    end
+  end
+  str
+end
+
+@page.visit 'https://chouseisan.com/'
+
+base_date = Date.today.next_week.beginning_of_week
+@page.fill_in :name, with: "第#{num(base_date)}回 Bio x IT輪読会"
+@page.fill_in :kouho, with: kouho(base_date)
+@page.find(:css, '#createBtn').click
+
+p @page.current_url # complete page
+
+p @page.find(:css, 'input').value
+# => "https://chouseisan.com/schedule/List?h=4d06d87f127d4dae8fd2634764375720201409"
