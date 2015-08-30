7749df41a0c2f1120f6a20bbf947283c9d500435
memerelics <takuya21hashimoto@gmail.com>
2015-08-30 22:21:51 +0900

    tweak method positions

--- megurolib.rb
+++ megurolib.rb
@@ -21,6 +21,19 @@ class MeguroLib < Base
     return Result.new(s)
   end
 
+  def login
+    top
+    s.find(:xpath, '//*[@id="km_user_widget-2"]/div/a[2]/img').click
+    s.fill_in :usercardno, with: conf['card']
+    s.fill_in :userpasswd, with: conf['password']
+    s.find(:xpath, '//table[4]//tr[2]/td[1]/table//tr[3]/td/input[1]').click
+    delay
+    unless my_page?
+      logger.error("[#{__method__}] Login failed.")
+      return false
+    end
+  end
+
   # 借りている資料
   def borrowing
     login
@@ -29,6 +42,40 @@ class MeguroLib < Base
      Book::Borrowing.new(*splite_title(cells[2]),
                          borrowed_at: cells[4], due: cells[6])
     end
+  end
+
+  # 予約している資料
+  def reserving
+    login
+    reserving_table.rows.map do |row|
+      cells = row.cells.map(&:text)
+      Book::Reserving.new(*splite_title(cells[2]), status: cells[4],
+                          reserved_at: cells[8], reserved_until: cells[5])
+    end
+  end
+
+  # TODO: sessionまわりを整頓して連続予約
+  # TODO: シリーズ予約に対応
+  def reserve(sess, book)
+    cd sess
+    puts "reserve: #{book.title} (#{book.publisher})"
+
+    target_link = s.all(:xpath, '//table[7]/tbody/tr').drop(1).map do |row|
+      if row.find(:css, 'td:nth-child(2)').text.include?(book.title) &&
+        row.find(:css, 'td:nth-child(4)').text.include?(book.publisher)
+        row.find(:css, 'a')
+      end
+    end.find{|a| !a.nil? }
+    target_link.click
+    delay
+    s.click_link '予約の候補にする'
+    delay
+    s.fill_in :usercardid, with: conf['card']
+    s.fill_in :password, with: conf['password']
+    s.select '3', from: 'library' # 中目黒駅前図書館
+    s.click_button '登録する'
+    delay
+    s.click_button '予約する'
   end
 
   # in detail page
@@ -103,53 +150,6 @@ class MeguroLib < Base
     end
 
     logger.debug("after: #{scan}")
-  end
-
-  # 予約している資料
-  def reserving
-    login
-    reserving_table.rows.map do |row|
-      cells = row.cells.map(&:text)
-      Book::Reserving.new(*splite_title(cells[2]), status: cells[4],
-                          reserved_at: cells[8], reserved_until: cells[5])
-    end
-  end
-
-  def login
-    top
-    s.find(:xpath, '//*[@id="km_user_widget-2"]/div/a[2]/img').click
-    s.fill_in :usercardno, with: conf['card']
-    s.fill_in :userpasswd, with: conf['password']
-    s.find(:xpath, '//table[4]//tr[2]/td[1]/table//tr[3]/td/input[1]').click
-    delay
-    unless my_page?
-      logger.error("[#{__method__}] Login failed.")
-      return false
-    end
-  end
-
-  # TODO: sessionまわりを整頓して連続予約
-  # TODO: シリーズ予約に対応
-  def reserve(sess, book)
-    cd sess
-    puts "reserve: #{book.title} (#{book.publisher})"
-
-    target_link = s.all(:xpath, '//table[7]/tbody/tr').drop(1).map do |row|
-      if row.find(:css, 'td:nth-child(2)').text.include?(book.title) &&
-        row.find(:css, 'td:nth-child(4)').text.include?(book.publisher)
-        row.find(:css, 'a')
-      end
-    end.find{|a| !a.nil? }
-    target_link.click
-    delay
-    s.click_link '予約の候補にする'
-    delay
-    s.fill_in :usercardid, with: conf['card']
-    s.fill_in :password, with: conf['password']
-    s.select '3', from: 'library' # 中目黒駅前図書館
-    s.click_button '登録する'
-    delay
-    s.click_button '予約する'
   end
 
   private def my_page?
